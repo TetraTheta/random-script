8a633fdc62815c2e4ad5e214d41b503af4b6c09b
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2024-12-02 15:50:00 +0900

    [RemoveEmptyDirectory] Add RemoveEmptyDirectory and fix few problems

---
+++ red.cmd
@@ -0,0 +1,3 @@
+@REM Remove empty directories
+@echo off
+python "%~dp0\script\remove_empty_directory.py" %*
--- script/remove_empty_directory.py
+++ script/remove_empty_directory.py
@@ -4,14 +4,15 @@ import sys
 from pathlib import Path
 
 
-def lowercase(dir: Path):
-    items = sorted(Path(dir).rglob("*"), key=lambda p: len(p.parts), reverse=True)
-    for item in items:
-        new_name = item.name.lower()
-        if item.name != new_name:
-            new_path = item.with_name(new_name)
-            print(f"{Fore.YELLOW}RENAME{Style.RESET_ALL} {item.parent}{os.sep}{{{Fore.YELLOW}{item.name}{Style.RESET_ALL} → {Fore.YELLOW}{new_name}{Style.RESET_ALL}}}")
-            item.rename(new_path)
+def remove_empty_directory(dir: Path):
+    for subdir in dir.iterdir():
+        if subdir.is_dir():
+            remove_empty_directory(subdir)
+    try:
+        dir.rmdir()
+        print(f"{Fore.YELLOW}REMOVE{Style.RESET_ALL} {dir.parent}{os.sep}{Fore.YELLOW}{dir.name}{Style.RESET_ALL}")
+    except OSError:
+        pass
 
 
 if __name__ == "__main__":
@@ -47,9 +48,9 @@ if __name__ == "__main__":
         print(f"{Fore.GREEN}TARGET{Style.RESET_ALL} {args.target}")
         result = input("Do you want to lowercase any sub directory or files of this directory? (yes/no): ")
         if result.lower() == "yes" or result.lower() == "y":
-            lowercase(args.target)
+            remove_empty_directory(args.target)
         else:
             print(f"{Fore.RED}ERROR{Style.RESET_ALL} User canceled the opration")
             sys.exit(1)
     else:
-        lowercase(args.target)
+        remove_empty_directory(args.target)
