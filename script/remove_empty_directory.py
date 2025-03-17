f69e882df6f2397185e6648af0f331cc8be42798
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2025-03-17 16:52:12 +0900

    Update 'remove_empty_directory' Since I need to use this in macOS, I've added support for macOS too.

--- script/remove_empty_directory.py
+++ script/remove_empty_directory.py
@@ -2,21 +2,27 @@ import os
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
-import stat
 
 from library.python_lib import Color, CustomFormatter  # noqa: E402
 
+# This script requires > 3.12 (Path.is_junction())
+if sys.version_info < (3, 12):
+    print(f"{Color.RED}ERROR  {Color.RESET} This script requires Python version above 3.12")
+    sys.exit(1)
 
-exclusion = [
-    r"%AppData%\\Microsoft",
-    r"%LocalAppData%\\Microsoft",
-]
+if sys.platform == "win32":
+    exclusion = [
+        r"%AppData%\\Microsoft",
+        r"%LocalAppData%\\Microsoft",
+    ]
+else:
+    exclusion = []
 exclusion = [Path(os.path.expandvars(path)).resolve() for path in exclusion]
 
 
 class RemoveEmptyDirectoryNamespace:
-    target: str | Path
-    yes: bool
+    target: str | Path = Path.cwd()
+    yes: bool = False
 
 
 def remove_empty_directory(dir: Path, top_level: Path):
@@ -24,24 +30,27 @@ def remove_empty_directory(dir: Path, top_level: Path):
         if not dir.is_dir():
             return
 
-        is_junction = stat.S_ISLNK(os.stat(dir, follow_symlinks=False).st_mode)
+        is_junction = dir.is_junction() if sys.platform == "win32" else False
         is_symlink = dir.is_symlink()
 
         if is_symlink or is_junction:
-            print(f"{Color.BLUE}SYMLNK{Color.RESET} {dir}")
+            print(f"{Color.BLUE}SYMLINK{Color.RESET} {dir}")
             return
 
         for subdir in dir.iterdir():
             if subdir.is_dir():
-                if any(subdir.resolve().is_relative_to(exc) for exc in exclusion):
-                    print(f"{Color.BLUE}SKIP  {Color.RESET} {subdir}")
-                    continue
+                try:
+                    if any(subdir.resolve().is_relative_to(exc) for exc in exclusion):
+                        print(f"{Color.BLUE}SKIP   {Color.RESET} {subdir}")
+                        continue
+                except OSError:
+                    pass
             remove_empty_directory(subdir, top_level)
 
         if dir != top_level:
             try:
                 dir.rmdir()
-                print(f"{Color.YELLOW}REMOVE{Color.RESET} {dir.parent}{os.sep}{Color.YELLOW}{dir.name}{Color.RESET}")
+                print(f"{Color.YELLOW}REMOVE {Color.RESET} {dir.parent}{os.sep}{Color.YELLOW}{dir.name}{Color.RESET}")
             except OSError:
                 pass
 
@@ -62,16 +71,16 @@ args = cli.parse_args(namespace=RemoveEmptyDirectoryNamespace)
 args.target = Path(args.target).resolve()
 
 if not args.target.is_dir():
-    print(f"{Color.RED}ERROR{Color.RESET} Given path '{args.target}' is not a directory")
+    print(f"{Color.RED}ERROR  {Color.RESET} Given path '{args.target}' is not a directory")
     sys.exit(1)
 
 if not args.yes:
     print(f"{Color.GREEN}TARGET{Color.RESET} {args.target}")
-    result = input("Do you want to lowercase any sub directory or files of this directory? (yes/no): ")
+    result = input("Do you want to remove empty directories from this path? (yes/no): ")
     if result.lower() == "yes" or result.lower() == "y":
-        remove_empty_directory(args.target)
+        remove_empty_directory(args.target, args.target)
     else:
-        print(f"{Color.RED}ERROR{Color.RESET} User canceled the opration")
+        print(f"{Color.RED}CANCEL {Color.RESET} User canceled the operation")
         sys.exit(1)
 else:
     remove_empty_directory(args.target, args.target)
